6dd8b6426612c2c5bf0d48ee58a18047d7a781a7
jasonthchan <teckhanc@gmail.com>
2021-09-16 02:47:26 +1000

    refactor

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 
 const todoName = [];
 const items = [];
@@ -40,27 +42,8 @@ app.post("/create", function(req, res) {
   res.redirect("/");
 })
 
-// app.get("/list", function(req, res) {
-//   res.render("list", {todoName: todoName, items: items, timeStamp: timeDay});
-// })
-// //
-// app.post("/list", function(req, res) {
-//   if (req.body.create === "list") {
-//     var todoListName = req.body.todoName;
-//     todoName.push(todoListName);
-//     res.redirect("/list");
-//   } else if (req.body.create === "item") {
-//     var todoListItem = req.body.items;
-//     items.push(todoListItem);
-//     res.redirect("/list");
-//   } else {
-//     console.log(error);
-//   }
-// })
-
 app.get("/list/:id", function(req, res) {
   let x = _.lowerCase(req.params.id);
-  console.log(x);
   lists.forEach(function(list) {
     let y = _.lowerCase(list.title);
     if (y === x) {
@@ -73,7 +56,6 @@ app.get("/list/:id", function(req, res) {
 
 app.post("/list/:id", function(req, res) {
   let x = _.lowerCase(req.params.id);
-  // console.log(req.params.id);
   let z = req.body.items
   lists.forEach(function(list) {
     let y = _.lowerCase(list.title);
@@ -84,15 +66,7 @@ app.post("/list/:id", function(req, res) {
       console.log("error");
     }
   })
-  // const list = {
-  //   title: req.params.id,
-  //   content: [req.body.items]
-  // }
-  // lists.push(list);
-  // console.log(list);
-  // res.redirect("/list/:id")
 })
-
 
 app.listen(3000, function() {
   console.log("server started and listening on port 3000");
